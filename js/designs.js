33abf293e9da0b9bdd379dc365094464cd59d8a6
Zineb <zineb.aitaaddi@gmail.com>
2018-06-30 22:34:22 +0100

    Add comments to Js code

--- js/designs.js
+++ js/designs.js
@@ -1,58 +1,68 @@
+// References used:
+    // *jQuery API: https://api.jquery.com/
+    // *jQuery Event Methods: https://www.w3schools.com/Jquery/jquery_ref_events.asp
+    // *jQuery HTML / CSS Methods: https://www.w3schools.com/Jquery/jquery_ref_html.asp
+
 $(document).ready(function(){
-    
-    
-        $('#sizePicker').submit(function() {
-            return false;
-        });
-    
-        let height = 20;
-        let width = 20;
-        makeGrid(height, width)
-    
-        $('#button_submit').click(function() {
-            height = parseInt($('#input_height').val());
-            width = parseInt($('#input_width').val());
-            
-            if (height && width) {
-                makeGrid(height, width)
-            }
-        });
-        
-    
+    // Set the default height and width and make the initial grid
+    let height = 20;
+    let width = 20;
+    makeGrid();
 
-        $('#button_reset').on('click', function() {
-            if (height && width) {
-                makeGrid(height, width)
-            }
-        })
-    
-        function makeGrid(h, w) {
-            $('#pixel_canvas').empty();
-            for (let i = 0; i < h; i++) {
-                let gridColumns = "";
-                for (let j = 0; j < w; j++) {
-                    gridColumns += '<td></td>';
-                }
-                $('#pixel_canvas').append('<tr>' + gridColumns + '</tr>');
-            }
+    // Bind a click event on the form submit button
+    $('#button_submit').click(function(event) {
+        // Prevent form submission to stop page refresh
+        event.preventDefault();
+        // Grab the height and width input values
+        height = $('#input_height').val();
+        width = $('#input_width').val();
+        // Check if the input values are in the right ranges
+        // then proceed to make the grid
+        if ((height > 0 && height <= 60)
+            && (width > 0 && width <= 80)) {
+            makeGrid();
         }
-    
+    });
 
-        
-        let color = $('#colorPicker');
-    
-        $(document).on("mousedown", "tr td", function () {
-            let paintColor = color.val();
+    // Bind a click event on the Clear button
+    $('#button_reset').on('click', function() {
+        // Reset the color of all grid cells
+        $('td').css('background-color', "white");
+    });
+
+    // Define makeGrid() function that creates grid cells
+    function makeGrid() {
+        // Empty the canvas
+        $('#pixel_canvas').empty();
+        // Canvas Constructor Loops
+        // Outer loop to create grid rows based on height
+        for (let i = 0; i < height; i++) {
+            let gridColumns = "";
+            // Inner loop to create grid cells based on width
+            for (let j = 0; j < width; j++) {
+                gridColumns += '<td></td>';
+            }
+            // Add new row to the grid at the end of each outer loop
+            $('#pixel_canvas').append('<tr>' + gridColumns + '</tr>');
+        }
+    }
+
+    $('#pixel_canvas').on("mousedown", "tr td", function () {
+        // Grab the selected color
+        let paintColor = $('#colorPicker').val();
+        // Fill cell with the color input
+        $(this).css('background-color', paintColor);
+        // Fill other cells on mouse over while mouse button is clicked
+        // and stop when it's not
+        $('tr td').bind("mousemove", function () {
             $(this).css('background-color', paintColor);
-            $('tr td').bind("mousemove", function () {
-                let paintColor = color.val();
-                $(this).css('background-color', paintColor);
-            }).mouseup(function() {
-                $('td').unbind('mousemove');
-            });
-            $('tr td').on('dblclick',function () {
-                $(this).css('background-color', "white")
-            })
+        }).mouseup(function() {
+            $('td').unbind('mousemove');
         });
     });
-    
+
+    // Removes color from cell upon double-click
+    $('tr td').on('dblclick',function () {
+        $(this).css('background-color', "white");
+    });
+});  
